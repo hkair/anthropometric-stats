f8fe84cf4b218fbcabfcfc8ab4af54561d881f91
hobin <hb2kang@gmail.com>
2022-05-08 04:16:17 -0400

    Add race and height selection, pie chart, correlation matrix and summary statistics

--- analysis/ansur_dash.py
+++ analysis/ansur_dash.py
@@ -4,6 +4,10 @@
 from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import plotly.figure_factory as ff
+import plotly.graph_objects as go
+from plotly.graph_objects import Layout
+from plotly.validator_cache import ValidatorCache
+from dash_table import DataTable
 
 import numpy as np
 import pandas as pd
@@ -27,7 +31,7 @@ drop_list_nonnumeric = ["Date", "Installation", "Component","PrimaryMOS"]
 df.drop(drop_list_nonnumeric, axis=1, inplace = True)
 
 ## Data Cleaning
-NaN_list =[]
+NaN_list = []
 for columns in df.columns:
     if df[columns].isnull().sum() > 0:
         print("{name} = {qty}".format(name = columns, qty = df[columns].isnull().sum()))
@@ -36,6 +40,72 @@ for columns in df.columns:
 df = df.drop(NaN_list, axis=1)
 df.drop("SubjectNumericRace", axis = 1, inplace = True)
 
+# Race 
+race_code = {"White":1, "Black":2, "Hispanic":3, "Asian":4, "Native American":5, "Pacific Islander":6, "Other":8}
+races = (["All", "White", "Black", "Hispanic", "Asian", "Native American", "Pacific Islander", "Other"])
+
+# Height
+heights = ["All"]
+heights.extend([str(55+i)+"-"+str(55+i+1) for i in range(35)])
+
+# Summary Stats Functions
+def percentiles_df(df, measure):
+    # measure - the measurement or column in the dataframe
+    k_percentiles = [1, 2, 3, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 97, 98, 99]
+    
+    male = df[df['Gender'] == "Male"][measure]
+    female = df[df['Gender'] == "Female"][measure]
+
+    print("PERCENTILES - ", measure)
+    print("FEMALES ", "MALES ")
+    
+    percentile_df = []
+    for k in k_percentiles:
+        if k == 1:
+            print(female.quantile(k*0.01), " {k}ST ".format(k=k), male.quantile(k*0.01))
+            d = {
+                'FEMALES': female.quantile(k*0.01),
+                'Percentile':  " {k}ST ".format(k=k),
+                'MALES': male.quantile(k*0.01)
+            }
+        elif k in [2, 3]:
+            print(female.quantile(k*0.01), " {k}ND ".format(k=k), male.quantile(k*0.01))
+            d = {
+                'FEMALES': female.quantile(k*0.01),
+                'Percentile':  " {k}ND ".format(k=k),
+                'MALES': male.quantile(k*0.01)
+            }
+        else:
+            print(female.quantile(k*0.01), " {k}TH ".format(k=k), male.quantile(k*0.01))
+            d = {
+                'FEMALES': female.quantile(k*0.01),
+                'Percentile':  " {k}TH ".format(k=k),
+                'MALES': male.quantile(k*0.01)
+            }
+        percentile_df.append(d)
+    return pd.DataFrame(percentile_df)
+
+def frequency_table(df, measure):
+    
+    iw = (df[measure].max()-df[measure].min())/10
+    data = df[measure]
+    n = len(data)
+    
+    # the number of bins is based on the frequency tables in the original ANSUR II dataset summary
+    frequency, intervals = np.histogram(data, bins = 40)
+    
+    freq = pd.DataFrame(index = np.linspace(1,40,40), columns = ['start', 'end', 'F'])
+    # Assign the intervals
+    freq['start'] = intervals[:-1]
+    freq['end'] = intervals[1:]
+    # Assing Absolute frecuency
+    freq['F'] = frequency
+    freq["Fpct"] = freq['F']/n
+    freq["CumF"] = freq['F'].cumsum()
+    freq["CumFPct"] = freq['CumF']/n
+    
+    return freq
+    
 # APP
 app = Dash(__name__)
 
@@ -43,18 +113,6 @@ colors = {
     'background': '#CAD7DA',
     'text': '#000000'
 }
-
-def generate_table(dataframe, max_rows=10):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-    ])
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     
@@ -78,20 +136,34 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         html.Div([
             dcc.Dropdown(
                 ["Female", "Male", "Both"],
-                'Female',
+                'Both',
                 id='gender'
             )
-        ], style={'width': '48%', 'display': 'inline-block'}),
+        ], style={'width': '25%', 'display': 'inline-block'}),
+        html.Div([
+            dcc.Dropdown(
+                races,
+                'All',
+                id='race'
+            )
+        ], style={'width': '25%', 'display': 'inline-block'}),
+        html.Div([
+            dcc.Dropdown(
+                heights,
+                'All',
+                id='height'
+            )
+        ], style={'width': '25%', 'display': 'inline-block'}),
         html.Div([
             dcc.Dropdown(
                 df.columns,
                 df.columns[0],
                 id='variable'
             )
-        ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+        ], style={'width': '25%', 'float': 'right', 'display': 'inline-block'})
     ]),
     
-    # Graph
+    # Distribution Graph
     dcc.Graph(id='graph'),
     
     # Percentile Slider
@@ -116,39 +188,113 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     ),
     html.Div(id='output-value', style={'backgroundColor': "#008B8B"}),
     
+    # Summary Stats
     html.Div(children='Summary Stats', style={
         'textAlign': 'center',
         'color': colors['text']
     }),
     
-#     html.Div([
-#         html.H4(children='ANSUR II (2012)'),
-#         generate_table(df)
-#     ])
+    html.Div(id='mean', children='MEAN: ', style={'backgroundColor': "#99E863"}),
+    html.Div(id='std-error', children='STD ERROR (MEAN): ',style={'backgroundColor': "#99E863"}),
+    html.Div(id='std-deviation', children='STANDARD DEVIATION: ', style={'backgroundColor': "#99E863"}),
+    html.Div(id='std-error-2', children='STD ERROR (STD DEV): ', style={'backgroundColor': "#99E863"}),
+    html.Div(id='min', children='MIN: ', style={'backgroundColor': "#99E863"}),
+    html.Div(id='max', children='MAX: ', style={'backgroundColor': "#99E863"}),
+    html.Div(id='skewness', children='SKEWNESS: ', style={'backgroundColor': "#99E863"}),
+    html.Div(id='kurtosis', children='KURTOSIS: ', style={'backgroundColor': "#99E863"}),
+    html.Div(id='coefficient', children='COEFFICIENT OF VARIATION: ', style={'backgroundColor': "#99E863"}),
+    html.Div(id='participants', children='NUMBER OF PARTICIPANTS: ', style={'backgroundColor': "#99E863"}),
+    
+    html.Div(children='Percentile', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+    
+    DataTable(
+        id='table',
+        data=[]
+    ),
+    
+    html.Div(children='Frequency Table', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+    
+    # Frequency Plot
+    DataTable(
+        id='frequency',
+        data=[]
+    ),
+    
+    # Correlation Matrix
+    html.Div(children='Correlation Matrix', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+    dcc.Graph(id="correlation-graph"),
+    html.P("Variables: "),
+    dcc.Checklist(
+        id='correlation-variables',
+        options=df.columns.values,
+        value=df.columns.values,
+    ),
+    
+    # Pie Chart
+    html.Div(children='Pie Chart', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+    dcc.Dropdown(
+        ['DODRace', 'Age', 'Branch', 'SubjectsBirthLocation'],
+        'DODRace',
+        id='names'
+    ),
+    
+    dcc.Graph(id="pie-chart"),
 ])
 
 # Callback for updating the graph
 @app.callback(
     Output('graph', 'figure'),
     Input('gender', 'value'),
+    Input('race', 'value'),
+    Input('height', 'value'),
     Input('variable', 'value'),
     Input('percentile', 'value'))
-def update_graph(gender, variable, percentile):
+def update_graph(gender, race, height, variable, percentile):
+    # Gender
     if gender == "Both":
-#        dff = df.copy()
-#         fig = px.histogram(dff, x=variable, color="Gender",
-#                    marginal="box")
-        hist_data = [df[df['Gender'] == "Male"][variable], df[df['Gender'] == "Female"][variable]]
+        # Race
+        if race != "All":
+            dff = df[df['DODRace'] == race_code[race]]
+        else:
+            dff = df.copy()
+        
+        # Height
+        if height != "All":
+            h1 = int(height[:2])
+            h2 = int(height[-2:])
+            dff = dff[(dff["Heightin"] >= h1) & (dff["Heightin"] <= h2)]
+            
+        hist_data = [dff[dff['Gender'] == "Male"][variable], dff[dff['Gender'] == "Female"][variable]]
         group_labels = ["Male", "Female"]
         fig = ff.create_distplot(hist_data, group_labels, colors=['blue', 'red'])
         
         # plot the vertical line
-        fig.add_vline(x=df[variable].quantile(percentile*0.01), line_width=3, line_dash="dash", line_color="green")
+        fig.add_vline(x=dff[variable].quantile(percentile*0.01), line_width=3, line_dash="dash", line_color="green")
         
     else:
-        dff = df[df['Gender'] == gender]
-#         fig = px.histogram(dff, x=variable,
-#                    marginal="box")
+        if race != "All":
+            dff = df[(df['Gender'] == gender) & (df['DODRace'] == race_code[race])]
+        else:
+            dff = df.copy()
+            
+        # Height
+        if height != "All":
+            h1 = int(height[:2])
+            h2 = int(height[-2:])
+            dff = dff[(dff["Heightin"] >= h1) & (dff["Heightin"] <= h2)]
+            
         hist_data = [dff[variable]]
         group_labels = [variable]
         if gender == "Male":
@@ -158,7 +304,7 @@ def update_graph(gender, variable, percentile):
         
         # plot the vertical line
         fig.add_vline(x=dff[variable].quantile(percentile*0.01), line_width=3, line_dash="dash", line_color="green")
-        
+    
     fig.update_xaxes(title=variable)
     fig.update_yaxes(title="density")
     
@@ -171,17 +317,158 @@ def update_graph(gender, variable, percentile):
 
     return fig
 
+# Callback for Summary Stats
+@app.callback(
+    [Output('mean', 'children'), 
+     Output('std-error', 'children'), 
+     Output('std-deviation', 'children'), 
+     Output('std-error-2', 'children'), 
+     Output('min', 'children'), 
+     Output('max', 'children'), 
+     Output('skewness', 'children'), 
+     Output('kurtosis', 'children'), 
+     Output('coefficient', 'children'), 
+     Output('participants', 'children')],
+    Input('gender', 'value'),
+    Input('race', 'value'),
+    Input('height', 'value'),
+    Input('variable', 'value'))
+def update_summary(gender, race, height, variable):
+        
+    # Race
+    if race != "All":
+        dff = df[df['DODRace'] == race_code[race]]
+    else:
+        dff = df.copy()
+        
+    if gender != "Both":
+        dff = dff[dff['Gender'] == gender]
+
+    # Height
+    if height != "All":
+        h1 = int(height[:2])
+        h2 = int(height[-2:])
+        dff = dff[(dff["Heightin"] >= h1) & (dff["Heightin"] <= h2)]
+    
+    dff = dff[variable]
+    
+    return "MEAN: " + str(np.mean(dff)), \
+           "STD ERROR (MEAN): " + str(scipy.stats.sem(dff)), \
+           "STANDARD DEVIATION: " + str(np.std(dff)), \
+           "STD ERROR (STD DEV): " + str(np.sqrt(np.sqrt(2*np.power(np.std(dff), 4)/(len(dff)-1)))), \
+           "MIN: " + str(np.min(dff)), \
+           "MAX: " + str(np.max(dff)), \
+           "SKEWNESS: " + str(scipy.stats.skew(dff)), \
+           "KURTOSIS: " + str(scipy.stats.kurtosis(dff, fisher=False)), \
+           "COEFFICIENT OF VARIATION: " + str(scipy.stats.variation(dff)), \
+           "NUMBER OF PARTICIPANTS: " + str(len(dff))
+
+# Callback for Summary Stats - Percentile
+@app.callback(
+    [Output("table", "data"), Output('table', 'columns')],
+    [Input('race', 'value'),
+    Input('height', 'value'),
+    Input('variable', 'value')]
+)
+def updateTable(race, height, variable):
+    # Race
+    if race != "All":
+        dff = df[df['DODRace'] == race_code[race]]
+    else:
+        dff = df.copy()
+
+    # Height
+    if height != "All":
+        h1 = int(height[:2])
+        h2 = int(height[-2:])
+        dff = dff[(dff["Heightin"] >= h1) & (dff["Heightin"] <= h2)]
+    
+    dff = percentiles_df(dff, variable)
+    return dff.to_dict('records'), tuple([ {'id': p, 'name': p} for p in dff.columns])
+
+# Callback for Frequency Table
+@app.callback(
+    [Output("frequency", "data"), Output('frequency', 'columns')],
+    [Input('race', 'value'),
+    Input('height', 'value'),
+    Input('variable', 'value')]
+)
+def updateFrequency(race, height, variable):
+    # Race
+    if race != "All":
+        dff = df[df['DODRace'] == race_code[race]]
+    else:
+        dff = df.copy()
+
+    # Height
+    if height != "All":
+        h1 = int(height[:2])
+        h2 = int(height[-2:])
+        dff = dff[(dff["Heightin"] >= h1) & (dff["Heightin"] <= h2)]
+    
+    dff = frequency_table(dff, variable)
+    return dff.to_dict('records'), tuple([ {'id': p, 'name': p} for p in dff.columns])
+
+    
+# Callback for updating Correlation Matrix
+@app.callback(
+    Output("correlation-graph", "figure"), 
+    Input("correlation-variables", "value"))
+def filter_heatmap(cols):
+    fig = px.imshow(df[cols].corr())
+    # Color
+    fig.update_layout(
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        font_color=colors['text']
+    )
+    return fig
+
+# Callback for updating Pie Chart
+@app.callback(
+    Output("pie-chart", "figure"), 
+    Input("names", "value"))
+def generate_chart(names):
+    labels = df[names].value_counts().index
+    values = df[names].value_counts().values
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='label+percent',
+                             insidetextorientation='radial'
+                            )])
+   
+    # Color
+    fig.update_layout(
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        font_color=colors['text']
+    )
+        
+    return fig
+
 # Callback for Value Slider
 @app.callback([Output(component_id='value_slider', component_property='min'),
                Output(component_id='value_slider', component_property='max'),
               Output(component_id='value_slider', component_property='value')],
               [Input('gender', 'value'),
+               Input('race', 'value'),
+               Input('height', 'value'),
                Input('variable', 'value')]) # This is the dropdown for selecting variable
-def update_value_slider(gender, variable):
+def update_value_slider(gender, race, height, variable):
     if gender == "Both":
-        dff = df.copy()
+        if race != "All":
+            dff = df[df['DODRace'] == race_code[race]]
+        else:
+            dff = df.copy()
     else:
-        dff = df[df['Gender'] == gender]
+        if race != "All":
+            dff = df[(df['Gender'] == gender) & (df['DODRace'] == race_code[race])]
+        else:
+            dff = df[df['Gender'] == gender]
+    
+    # Height
+    if height != "All":
+        h1 = int(height[:2])
+        h2 = int(height[-2:])
+        dff = dff[(dff["Heightin"] >= h1) & (dff["Heightin"] < h2)] 
 
     return dff[variable].min(), dff[variable].max(), (dff[variable].min() + dff[variable].max())/2
 
@@ -189,28 +476,58 @@ def update_value_slider(gender, variable):
 @app.callback(
     Output(component_id='output-percentile', component_property='children'),
     Input('gender', 'value'),
+    Input('race', 'value'),
+    Input('height', 'value'),
     Input('variable', 'value'),
     Input('percentile', 'value')
 )
-def update_percentile_div(gender, variable, percentile):
+def update_percentile_div(gender, race, height, variable, percentile):
     if gender == "Both":
-        dff = df.copy()
+        if race != "All":
+            dff = df[df['DODRace'] == race_code[race]]
+        else:
+            dff = df.copy()
     else:
-        dff = df[df['Gender'] == gender]
-    return f'Percentile -> Value: {dff[variable].quantile(percentile*0.01)} (mm or kg)'
+        if race != "All":
+            dff = df[(df['Gender'] == gender) & (df['DODRace'] == race_code[race])]
+        else:
+            dff = df[df['Gender'] == gender]
+
+    # Height
+    if height != "All":
+        h1 = int(height[:2])
+        h2 = int(height[-2:])
+        dff = dff[(dff["Heightin"] >= h1) & (dff["Heightin"] < h2)] 
+        
+    return f'Percentile -> Value: {dff[variable].quantile(percentile*0.01)} (mm (inches) or kg (lbs))'
 
 # Callback for updating value->percentile output
 @app.callback(
     Output(component_id='output-value', component_property='children'),
     Input('gender', 'value'),
+    Input('race', 'value'),
+    Input('height', 'value'),
     Input('variable', 'value'),
     Input('value_slider', 'value'),
 )
-def update_value_div(gender, variable, value):
+def update_value_div(gender, race, height, variable, value):
     if gender == "Both":
-        dff = df.copy()
+        if race != "All":
+            dff = df[df['DODRace'] == race_code[race]]
+        else:
+            dff = df.copy()
     else:
-        dff = df[df['Gender'] == gender]
+        if race != "All":
+            dff = df[(df['Gender'] == gender) & (df['DODRace'] == race_code[race])]
+        else:
+            dff = df[df['Gender'] == gender]
+            
+    # Height
+    if height != "All":
+        h1 = int(height[:2])
+        h2 = int(height[-2:])
+        dff = dff[(dff["Heightin"] >= h1) & (dff["Heightin"] < h2)] 
+        
     return f'Value -> Percentile: {round(scipy.stats.percentileofscore(dff[variable], value), 2)}%'
     
 if __name__ == '__main__':
