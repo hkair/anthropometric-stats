a7aa86dc1827fff0fdf15661202b161a82ab1c49
hobin <hb2kang@gmail.com>
2022-05-18 00:37:45 -0400

    Add proportionality ratios to poseModule

--- poseModule.py
+++ poseModule.py
@@ -1,6 +1,7 @@
 import cv2 
 import mediapipe as mp
 import math
+import statistics
 
 class poseDetector:
     
@@ -43,7 +44,7 @@ class poseDetector:
     
     def getLengths(self, img):
         
-        joints = {"shoulder": [11, 12],
+        self.joints = {"shoulder": [11, 12],
                   "hip": [23, 24],
                   "left_humerus": [12, 14],
                   "right_humerus": [11, 13],
@@ -54,23 +55,62 @@ class poseDetector:
                   "left_tibia": [26, 28],
                   "right_tibia": [25, 27],
                  }
+        
         lengths = {}
-        for key, val in joints.items():
+        for key, val in self.joints.items():
             lengths[key] = self.distance(self.lmList[val[0]][1:], self.lmList[val[1]][1:])
             
         # calculate torso using midpoint of shoulder and hip
-        midpoint_shoulder = self.midpoint(self.lmList[joints["shoulder"][0]][1:], self.lmList[joints["shoulder"][1]][1:])
-        midpoint_hip = self.midpoint(self.lmList[joints["hip"][0]][1:], self.lmList[joints["hip"][1]][1:])
+        midpoint_shoulder = self.midpoint(self.lmList[self.joints["shoulder"][0]][1:], self.lmList[self.joints["shoulder"][1]][1:])
+        midpoint_hip = self.midpoint(self.lmList[self.joints["hip"][0]][1:], self.lmList[self.joints["hip"][1]][1:])
         lengths["torso"] = self.distance(midpoint_shoulder, midpoint_hip)
-        
-        print(lengths)
-        
+        self.lengths = lengths
+        return lengths
             
     def distance(self, p1, p2):
         return math.sqrt((p2[1] - p1[1]) ** 2 + (p2[0] - p1[0]) ** 2)
     
     def midpoint(self, p1, p2):
         return ((p1[0]+p2[0])/2, (p1[1]+p2[1])/2)
+    
+    def getHeadBox(self, img, draw=True):
+        
+        # And both years as width of face
+        width = self.distance(self.lmList[7][1:], self.lmList[8][1:])
+        # The ratio of width-to-height is approximately two third
+        height = width*(3/2)
+        
+        midpoint = self.midpoint(self.lmList[7][1:], self.lmList[8][1:])
+        
+        # head
+        pt1 = (int(midpoint[0]-(width/2)), int(midpoint[1]-(height/2)))
+        pt2 = (int(midpoint[0]+(width/2)), int(midpoint[1]+(height/2)))
+        
+        self.top_head = pt2
+
+        if draw:
+            cv2.rectangle(img, pt1=pt1, pt2=pt2, color=(0,0,255), thickness=10)
+        
+        return img
+    
+    def getProportions(self, img, draw=True):
+        
+        # get bottom most y-position
+        bottom_point = statistics.mean([self.lmList[29][2], self.lmList[30][2], self.lmList[31][2], self.lmList[32][2]])
+        height = abs(self.top_head[1] - bottom_point)
+        print(height)
+        
+        self.proportions = { key: round(val/height, 3) for key, val in self.lengths.items()}
+        
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        if draw:
+            for key, val in self.proportions.items():
+                if key in self.joints:
+                    i, j = self.joints[key]
+                    midpoint = self.midpoint(self.lmList[i][1:], self.lmList[j][1:])
+                    print(midpoint)
+                    cv2.putText(img, str(val), (int(midpoint[0]), int(midpoint[1])), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        return img
         
     
 def main():
@@ -80,18 +120,23 @@ def main():
 #     cap = cv2.VideoCapture("images/lebron.webp") # video capture source camera (Here webcam of laptop) 
 #     ret, frame = cap.read() # return a single frame in variable `frame`
     
-    img = cv2.imread("images/stephen-curry.png")
+    file = "images/lebron.webp"
+    img = cv2.imread(file)
     while True:
         img = detector.getPose(img)
         lmList = detector.getPosition(img)
         print(lmList)
-        detector.getLengths(img)
+        lengths = detector.getLengths(img)
+        print(lengths)
+        img = detector.getHeadBox(img)
+        
+        img = detector.getProportions(img)
         cv2.imshow('img', img) #display the captured image
         #if cv2.waitKey(1) & 0xFF == ord('y'): #save on pressing 'y' 
         cv2.imwrite('images/c1.png', img)
         cv2.destroyAllWindows()
         break
-
+    print(type(img))
     #cap.release()
     
     
